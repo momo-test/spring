5a6dfd96a74471ff560b4ff402652979619cdf7e
Mohamed Ayman <mohamed.ayman-cic@ibm.com>
2024-10-24 17:01:47 +0200

    add before all before the test

--- src/test/java/com/ibm/jumpstart/springcourse/SpringcourseApplicationTests.java
+++ src/test/java/com/ibm/jumpstart/springcourse/SpringcourseApplicationTests.java
@@ -1,22 +1,35 @@
 package com.ibm.jumpstart.springcourse;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringcourseApplicationTests {
 
+	private static Logger logger ; 
 	@Autowired
 	SpringcourseApplication springcourseApplication ;
+
+	@BeforeAll
+	static void intiallizing ( ) {
+		
+	logger = LoggerFactory.getLogger(SpringcourseApplicationTests.class) ; 
+	logger.info("logger has been intiallized ! ");
+
+	}
 
 	@Test
 	void contextLoads() {
 		
 	assertEquals( springcourseApplication.getActiveProfile() , "dev"); 
 
-	SpringcourseApplication.main(new String[]{"firstArg" , "secondArg", "fourthArg"});
+
+	SpringcourseApplication.main(new String[]{"firstArg" , "secondArg", "fourthArg"}) ;
 
 	}
 
