4450e0a00bd3c3d04ed688bf6851f04878ed2ba2
Mohamed Ayman <mohamed.ayman-cic@ibm.com>
2024-10-24 15:12:01 +0200

    add another active profile and switching between them in spring

--- src/main/java/com/ibm/jumpstart/springcourse/SpringcourseApplication.java
+++ src/main/java/com/ibm/jumpstart/springcourse/SpringcourseApplication.java
@@ -1,14 +1,23 @@
 package com.ibm.jumpstart.springcourse;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 import com.ibm.jumpstart.api.Controller;
 
+import jakarta.annotation.PostConstruct;
+
 @ComponentScan("com.ibm.jumpstart.api")
 @SpringBootApplication
+@PropertySource("classpath:dev.properties")
+
 public class SpringcourseApplication {
+
+	@Value ("${module.type}")
+	private  String activeModule ; 
 
 	public static Controller controller;
 
@@ -17,7 +26,11 @@ public class SpringcourseApplication {
 	 SpringApplication.run(SpringcourseApplication.class, args);	
 	
 	
-	
+	}
+	@PostConstruct
+	public void checkActiveProfile ( ){
+		System.out.println("Active Module : " + activeModule);
+
 	}
 
 }
